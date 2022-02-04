25282a302b77d469516300bf4a8b45b8f667ad18
Susumu Shikano <susumu.shikano@uni-konstanz.de>
2022-02-04 11:28:07 +0100

    Updatin the function be.convert.time

--- R/BallotExp_convert_time.R
+++ R/BallotExp_convert_time.R
@@ -14,19 +14,25 @@ be.convert.time <- function(start.var=NULL,
   if (is.null(end.var)) stop("Input the variable name for the end time")
   if (is.null(data)) stop("Input the data name")
   
-  time.stamps <- rep(NA,2)
   for (i in 1:2){
     if (i == 1) {
       this.time.stamp <- data[,start.var]
     }else{
       this.time.stamp <- data[,end.var]
     }
+    
     this.time.stamp <- gsub("Z","",this.time.stamp)
     this.time.stamp <- gsub("T"," ",this.time.stamp)
-    time.stamps[i] <- strptime(this.time.stamp, format = "%Y-%m-%d %H:%M:%OS")
+    this.time.stamp <- strptime(this.time.stamp, format = "%Y-%m-%d %H:%M:%OS")
+    
+    if (i == 1) {
+      start.time <- this.time.stamp 
+    }else{
+      end.time <- this.time.stamp 
+    }
   }
-
-  dur <- time.stamps[2] - time.stamps[1]
   
-  list(duration=dur,start.time=time.stamps[1],end.time=time.stamps[2])
+  dur <- end.time - start.time
+  
+  list(duration=dur,start.time=start.time,end.time=end.time)
 }
