44ecdcabf2192fc3af67feed937ba3fe320ae690
SuShikano <susumu.shikano@uni-konstanz.de>
2024-11-25 15:41:27 +0100

    Update

--- R/emp_dist_func.R
+++ R/emp_dist_func.R
@@ -9,19 +9,36 @@
 # ---------------------------------------------------------------------------- #
 # ##############################################################################
 # ##############################################################################
-emp.dist.func <- function(x){
-  freq.x <- table(x)
-  cum.freq.x <- cumsum(freq.x)/max(cumsum(freq.x))
-  x.axis <- c(0,cum.freq.x,1)
-  names(x.axis)[1] <- 0
-  names(x.axis)[length(x.axis)] <- length(x.axis)-1
+emp.dist.func <- function(x,xlab="x",lwd=2,main=NULL){
+  freq.x <- table(x) # frequency table
+  cum.freq.x <- cumsum(freq.x)/max(cumsum(freq.x)) # cumulative frequency
+  x.axis <- as.numeric(names(cum.freq.x))
+  x.axis <- c(min(x.axis)-1 , x.axis , max(x.axis)+1)
+  
+  
+  cum.freq.x <- c(0,cum.freq.x,1)
+  
 
-  x.lim <- c(min(as.numeric(names(x.axis)))+.5,max(as.numeric(names(x.axis)))+.5)
+  x.lim <- range(x.axis)
 
-  plot(names(cum.freq.x),cum.freq.x,type="s",ylim=c(0,1),
-       xlim=,
-       axes=F,xlab="Response",ylab="Relative Freq")
-  axis(1,at=as.numeric(names(cum.freq.x)))
-  axis(2,at=c(0,1/4,1/2,3/4,1))
+  plot(x.axis[2:(length(x.axis)-1)],cum.freq.x[2:(length(x.axis)-1)],pch=19,
+       xlim=x.lim,
+       ylim=c(0,1),
+       xlab=xlab,
+       ylab=expression(P(X <=  x)),
+       main=main)
+  par(new=T)
+  plot(x.axis[2:(length(x.axis)-1)],cum.freq.x[1:(length(x.axis)-2)],pch=1,
+       xlim=x.lim,
+       ylim=c(0,1),
+       ann=F,axes=F)
+  for (i in 1:(length(x.axis)-1)){
+    lines(x.axis[i:(i+1)],rep(cum.freq.x[i],2),lwd=lwd)
+  }
+  
+
+  
+  
+  
   }
 
