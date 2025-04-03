9af872acd227cabc59934044ac45d2d3701a06e4
SuShikano <susumu.shikano@uni-konstanz.de>
2025-04-03 12:31:09 +0200

    Update: clogit (added some help info)

--- R/clogit.R
+++ R/clogit.R
@@ -1,24 +1,43 @@
-clogit <- function(formula, data, group_var, alt_var) {
+# ---------------------------------------------------------------------------- #
+# ##############################################################################
+# ##############################################################################
+#' This function creates the empirical (cumulative) distribution function
+#'
+#' @param formular 
+#' @param data Data in long format. To create this data, 
+#'                    you can call wide2long.R and use wide2long()
+#' @param group.var
+#' @param alt.var
+#' @return list consisting of coefficients = estimates, 
+#'                            std.errors = se, 
+#'                            z.values = z.values, 
+#'                            p.values = p.values, 
+#'                            logLik = -fit$minimum
+#' @examples
+# ---------------------------------------------------------------------------- #
+# ##############################################################################
+# ##############################################################################
+clogit <- function(formula, data, group.var, alt.var) {
   # Extract response and predictors
   mf <- model.frame(formula, data)
   y <- model.response(mf)
   X <- model.matrix(formula, data)
   
-  # Create Alternative-Specific Constants (ASCs)
-  alt_levels <- unique(data[[alt_var]])
-  asc_matrix <- model.matrix(~ factor(data[[alt_var]]) - 1)  # Create dummies for alternatives
-  X <- cbind(asc_matrix[,-1], X[,-1])  # Combine ASCs with predictors
+  # Alternative-Specific Constants (ASCs)
+  alt.levels <- unique(data[[alt.var]])
+  asc.matrix <- model.matrix(~ factor(data[[alt.var]]) - 1)  # Dummies for alternatives
+  X <- cbind(asc.matrix[,-1], X[,-1])  # ASCs with predictors
   
   # Extract group information (each choice set)
-  groups <- data[[group_var]]
+  groups <- data[[group.var]]
   
   # Log-likelihood function
-  log_likelihood <- function(beta) {
-    eta <- X %*% beta  # Compute linear predictor
-    exp_eta <- exp(eta)
+  log.likelihood <- function(beta) {
+    eta <- X %*% beta  # Linear predictor
+    exp.eta <- exp(eta)
     
-    # Compute probability of each choice within its group
-    probs <- exp_eta / ave(exp_eta, groups, FUN = sum)
+    # Prob of each choice within its group
+    probs <- exp.eta / ave(exp.eta, groups, FUN = sum)
     
     # Log-likelihood
     loglik <- sum(y * log(probs))
@@ -26,15 +45,15 @@ clogit <- function(formula, data, group_var, alt_var) {
   }
   
   # Optimization using Newton-Raphson (via nlm)
-  start_vals <- rep(0, ncol(X))  # Initial values (zeros)
-  fit <- nlm(log_likelihood, p = start_vals, hessian = TRUE)
+  start.vals <- rep(0, ncol(X))  # Initial values (zeros)
+  fit <- nlm(log.likelihood, p = start.vals, hessian = TRUE)
   
   # Extract results
   estimates <- fit$estimate
   se <- sqrt(diag(solve(fit$hessian)))  # Standard errors from Hessian
-  z_values <- estimates / se  # Z-values
-  p_values <- 2 * (1 - pnorm(abs(z_values)))  # P-values
+  z.values <- estimates / se  # Z-values
+  p.values <- 2 * (1 - pnorm(abs(z.values)))  # P-values
   
   # Return as a list
-  return(list(coefficients = estimates, std_errors = se, z_values = z_values, p_values = p_values, logLik = -fit$minimum))
+  return(list(coefficients = estimates, std.errors = se, z.values = z.values, p.values = p.values, logLik = -fit$minimum))
 }
