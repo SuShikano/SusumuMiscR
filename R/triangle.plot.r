24ab9f10603853610fda659b791f00c0de8f7d61
SuShikano <susumu.shikano@uni-konstanz.de>
2025-07-14 12:30:53 +0200

    update: triangle plot

--- R/triangle.plot.r
+++ R/triangle.plot.r
@@ -13,18 +13,28 @@ triangle.plot <- function(raw.values,
                           label=c("A","B","C"),
                           title="",
                           col="black",
-                          pch=1){
+                          pch=1,
+                          label.space = 1){
    raw.sum <- apply(raw.values,1,sum)
    values <- raw.values / raw.sum
    horizontal <- (values[,2]-values[,1])*100
    vertical <- (sqrt(3)/2) * values[,3] * 100
    plot(horizontal,vertical,
-                   xlim=c(-100,100),ylim=c(0,(sqrt(3)/2)*100),
-                   xlab=paste(label[1],"<-    ->",label[2]),ylab=paste("->",label[3]),
+                   xlim=c(-100,100)*label.space,
+                   ylim=c(0-100*(label.space-1),
+                          (sqrt(3)/2)*100*label.space),
+                   xlab="",ylab="",
+                   #xlab=paste(label[1],"<-    ->",label[2]),ylab=paste("->",label[3]),
                    main=title,axes=F,
                    col=col,
                   pch=pch)
     lines(c(-100,0,100,-100),c(0,(sqrt(3)/2)*100,0,0))
+    lines(rep(0,2),c(0,(sqrt(3)/2)*100),lty=2)
+    lines(c(-100,50),c(0,(sqrt(3)/2)*50),lty=2)
+    lines(c(100,-50),c(0,(sqrt(3)/2)*50),lty=2)
+    text(-100,0,label[1],pos=1)
+    text(100,0,label[2],pos=1)
+    text(0,(sqrt(3)/2)*100,label[3],pos=3)
 }
 
 
